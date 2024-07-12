f9fe14ae0467cc06462da159f7b1d8cdc5537b6e
Jerome Sauret <sauret.jerome@gmail.com>
2024-07-12 08:22:05 +0200

    working constants

---
+++ main/constants.py
@@ -0,0 +1,17 @@
+# constants.py
+
+# Scores in text for the tennis game
+scores = dict(
+    LOVE="Love",
+    FIFTEEN="Fifteen",
+    THIRTY="Thirty",
+    FORTY="Forty",
+    DEUCE="Deuce",
+    ADVANTAGE_PLAYER1="Advantage player1",
+    ADVANTAGE_PLAYER2="Advantage player2",
+    WIN_FOR_PLAYER1="Win for player1",
+    WIN_FOR_PLAYER2="Win for player2",
+    LOVE_ALL="Love-All",
+    FIFTEEN_ALL="Fifteen-All",
+    THIRTY_ALL="Thirty-All"
+)
